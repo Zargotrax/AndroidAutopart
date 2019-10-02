a43be2cc3327b1c0a0b6dae44a94b7b4aaf6fe4a
Zkyzz <mamorneau@hotmail.ca>
2019-10-02 13:39:25 -0400

    Added Supplier Detail Page

---
+++ app/src/main/java/com/xample/androidautopart/SupplierDetailActivity.java
@@ -0,0 +1,14 @@
+package com.xample.androidautopart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SupplierDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supplier_detail);
+    }
+}
---
+++ app/src/main/java/com/xample/androidautopart/ui/supplier/Supplier.java
@@ -0,0 +1,8 @@
+package com.xample.androidautopart.ui.supplier;
+
+import java.io.Serializable;
+
+public class Supplier implements Serializable {
+    public String id, name, address_line_1, address_line_2, address_city, address_province, address_postal_code, telephone, contact;
+
+}
