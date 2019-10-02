9029857cf105b1f67051e5681d62c19191c2d32a
Zkyzz <mamorneau@hotmail.ca>
2019-10-02 16:10:57 -0400

    Build complete
    To be tested...

--- app/src/main/java/com/xample/androidautopart/ui/supplier/SupplierFragment.java
+++ app/src/main/java/com/xample/androidautopart/ui/supplier/SupplierFragment.java
@@ -15,7 +15,15 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.xample.androidautopart.R;
+
+import org.json.JSONArray;
 
 public class SupplierFragment extends Fragment {
 
@@ -40,5 +48,26 @@ public class SupplierFragment extends Fragment {
         recyclerView.setAdapter(SupplierViewModel.adapter);
 
         return root;
+    }
+
+    protected void doRequest() {
+        RequestQueue queue = Volley.newRequestQueue(this.getContext());
+
+        String url = "https://f6eb04bb.ngrok.io//suppliers";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        SupplierViewModel.GetList(response);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+        queue.add(jsonArrayRequest);
     }
 }
